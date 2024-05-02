ef4b424ebd365052f3af1fccc932e1dd23710571
Jerry Huang <jerry83314@gmail.com>
2024-05-02 17:55:54 +0900

    Navbar 使用 NavLink 取代 Link

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,27 +1,28 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export default function Navbar() {
     return (
         <>
           <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-              <a className="navbar-brand" href="#">Navbar</a>
+              <div className="navbar-brand">Navbar</div>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                   <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    <NavLink to="/about" className="nav-link" style={({ isActive }) => {
+                      return ({
+                        color: isActive ? 'red': ''
+                      })
+                    }}>關於</NavLink>
                   </li>
                   <li className="nav-item">
-                    <Link to="/about" className="nav-link">關於</Link>
+                    <NavLink to="/album" className="nav-link">相簿</NavLink>
                   </li>
                   <li className="nav-item">
-                    <Link to="/album" className="nav-link">相簿</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/album/search" className="nav-link">搜尋</Link>
+                    <NavLink to="/album/search" className="nav-link">搜尋</NavLink>
                   </li>
                   <li className="nav-item dropdown">
                     <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
