6f39b807499818c7443bbecebc1b4ffd974dbb62
Jerry Huang <jerry83314@gmail.com>
2024-05-09 17:16:54 +0900

    增加撰寫 scss 功能

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link, NavLink } from "react-router-dom"
 export default function Navbar() {
     return (
         <>
-          <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <nav className="navbar navbar-expand-lg bg-body-tertiary app">
             <div className="container-fluid">
               <div className="navbar-brand">Navbar</div>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- src/pages/reactClass/UseContext.jsx
+++ src/pages/reactClass/UseContext.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, createContext } from "react";
 import LoginForm from "./components/LoginForm";
 import Greeting from "./components/Greeting";
 import UserContext from "../../context/userContext";
+import bannerStyle from "../../assets/scss/banner.module.scss"
 
 export default function UseContext() {
   const [username, Setusername] = useState('');
@@ -10,7 +11,7 @@ export default function UseContext() {
   return(
     <>
       <UserContext.Provider value={{username, Setusername, setIsLogin}}>
-      <p>這是 useContext 頁面</p>
+      <p className={bannerStyle.hello_test}>這是 useContext 頁面</p>
       { isLogin ? <Greeting/> : <LoginForm/> }
       </UserContext.Provider>
     </>
